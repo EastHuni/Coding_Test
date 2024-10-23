cff94c48c3b46b5c1e83fd10b32bcf21f36a0b7f
easthuni <east_huni@naver.com>
2024-10-23 22:55:30 +0900

    수정

--- baekjoon/test.java
+++ baekjoon/test.java
@@ -6,5 +6,6 @@ public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("h_y");
+        System.out.println("d_h");
     }
 }
